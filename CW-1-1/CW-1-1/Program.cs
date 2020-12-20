9006ecdb698d1243877c4118969c50b847fa5fbe
ahmad-alshabani <ahmad-alshabani@users.noreply.github.com>
2020-12-20 19:25:31 +0300

    Excercise 1

---
+++ CW-1-1/CW-1-1/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace CW_1_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string characterName = "Ahmad";
+            string superPower1 = "Iron Sheild";
+            string superPower2 = "Night vision";
+            double speed = 1.65;
+            int age = 15;
+
+            Console.WriteLine("My name is {4}, and my superpowers are {3} and {2}. My age is {1} and my speed is {0} ", speed, age, superPower2, superPower1, characterName);
+        }
+    }
+}
