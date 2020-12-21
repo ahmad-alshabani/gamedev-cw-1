a0ba44345aa4922040a5823c3e7e63c431bca005
ahmad-alshabani <ahmad-alshabani@users.noreply.github.com>
2020-12-21 18:29:26 +0300

    Task 3
    Functions

---
+++ CW-1-2/Task 3/Task 3/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Task_3
+{
+    class Program
+    {
+        static double playerSpeed = 50;
+        static void Main(string[] args)
+        {
+            SetSpeed(2.5);
+            GetSpeed();
+        }
+
+        static void SetSpeed(double speed)
+        {
+            playerSpeed = speed;
+        }
+        static void GetSpeed()
+        {
+            Console.WriteLine("The current player speed is {0}", playerSpeed);
+        }
+    }
+}
