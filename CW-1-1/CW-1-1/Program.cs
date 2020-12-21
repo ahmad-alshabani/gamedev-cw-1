e393fc09b79850ddea97a702c1cd6e6fa2792879
ahmad-alshabani <ahmad-alshabani@users.noreply.github.com>
2020-12-21 17:52:41 +0300

    Task 2

--- CW-1-1/CW-1-1/Program.cs
+++ CW-1-1/CW-1-1/Program.cs
@@ -12,7 +12,18 @@ namespace CW_1_1
             double speed = 1.65;
             int age = 15;
 
+            string heroName = "Kratos";
+            int heroHeight = 188;
+            int heroAge = 35;
+            string heroSuperPower1 = "Blades of Athena";
+            string heroSuperPower2 = "Wrath of Valhalla";
+
+            int ageDifference = heroAge - age;
+
             Console.WriteLine("My name is {4}, and my superpowers are {3} and {2}. My age is {1} and my speed is {0} ", speed, age, superPower2, superPower1, characterName);
+            Console.WriteLine("And my name is {0}, and my superpowers are {1} and {2}. I am {3} years old, and I am standing at {4}cm. ", heroName, superPower1, superPower2, heroAge, heroHeight);
+            Console.WriteLine("The age difference between the two character is {0}" ,ageDifference);
+            if (heroSuperPower1 != superPower1) Console.WriteLine("There is a character that is stronger than the other one");
         }
     }
 }
